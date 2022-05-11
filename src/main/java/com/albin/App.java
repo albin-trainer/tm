7a1c5af83c798fe7ae871336b91bf41cd2460397
albin-trainer <albinxavier@gmail.com>
2022-05-11 07:33:46 +0530

    fff

--- src/main/java/com/albin/App.java
+++ src/main/java/com/albin/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Good morning");
         System.out.println(new App().add(5,5));
     }
   public  int add (int a,int b) {
