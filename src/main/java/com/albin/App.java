9883f5200a408a0b379a7bf6e73b2d2d310d83ca
albin-trainer <albinxavier@gmail.com>
2022-05-11 07:29:04 +0530

    g

---
+++ src/main/java/com/albin/App.java
@@ -0,0 +1,17 @@
+package com.albin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(new App().add(5,5));
+    }
+  public  int add (int a,int b) {
+    	return a+b;
+    }
+}
